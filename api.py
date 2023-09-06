d2bf8178a19879285bc748d6cd1abbd3ab7132ef
Shashank Mittal <117439240+ishashankmittal@users.noreply.github.com>
2023-09-06 23:54:47 +0530

    Update api.py

--- api.py
+++ api.py
@@ -49,4 +49,4 @@ def classify():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=8000)
+    app.run(debug=False,host='0.0.0.0')
